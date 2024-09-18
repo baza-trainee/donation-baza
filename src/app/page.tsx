50c8761657132f0a7dc2ca450a17938368431e73
Maxym Biriukov <biryukov.mm@gmail.com>
2024-09-18 18:08:13 +0300

    feat: add example of using every font variant from ui kit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Applications from '@/components/Applications';
 import SignUp from '@/components/SignUp';
 
+import styles from './Page.module.scss';
 import { validateRequest } from '@/lib/lucia';
 
 export default async function Home() {
@@ -10,6 +11,15 @@ export default async function Home() {
     <div className="wrapper">
       <pre>{JSON.stringify(user, null, 2)}</pre>
       <pre>{JSON.stringify(session, null, 2)}</pre>
+      <p className={styles.one}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.two}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.three}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.four}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.five}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.six}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.seven}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.eight}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles.nine}>Сила в єдності: твоя допомога важлива</p>
       <Applications />
       <SignUp />
     </div>
