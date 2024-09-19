5f36322289245ec8532cd7137329266aab0dbf98
Max <biryukov.mm@gmail.com>
2024-09-19 16:59:11 +0300

    chore: renamed classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,38 @@
-import Applications from '@/components/Applications';
 import Link from 'next/link';
 
 import styles from './Page.module.scss';
 
 export default function Home() {
   return (
-    <div className="wrapper">
-      <Link href={'/login'}>login</Link>
-      <Link href={'/applications'}>applications</Link>
-      <Link href={'/admin'}>admin</Link>
-      <p className={styles.one}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.two}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.three}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.four}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.five}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.six}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.seven}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.eight}>Сила в єдності: твоя допомога важлива</p>
-      <p className={styles.nine}>Сила в єдності: твоя допомога важлива</p>
-      <Applications />
-    </div>
+    <>
+      <div className="wrapper">
+        <Link href={'/login'}>login</Link>
+        <Link href={'/applications'}>applications</Link>
+        <Link href={'/admin'}>admin</Link>
+      </div>
+      <p className={styles['text-h1']}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles['text-h2']}>Сила в єдності: твоя допомога важлива</p>
+      <p className={styles['text-subtitle']}>
+        Сила в єдності: твоя допомога важлива
+      </p>
+      <p className={styles['text-card-title']}>
+        Сила в єдності: твоя допомога важлива
+      </p>
+      <p className={styles['text-accent']}>
+        Сила в єдності: твоя допомога важлива
+      </p>
+      <p className={styles['text-main']}>
+        Сила в єдності: твоя допомога важлива
+      </p>
+      <p className={styles['text-big-button']}>
+        Сила в єдності: твоя допомога важлива
+      </p>
+      <p className={styles['text-medium-button']}>
+        Сила в єдності: твоя допомога важлива
+      </p>
+      <p className={styles['text-small']}>
+        Сила в єдності: твоя допомога важлива
+      </p>
+    </>
   );
 }
