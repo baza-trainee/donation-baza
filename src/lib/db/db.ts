14ec26a25fa1e61639103309f40eeab1530d9e87
cold <kholodihor@gmail.com>
2024-09-19 10:17:15 +0300

    fix: fix db connection

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,6 +2,16 @@
 /* eslint-disable no-var */
 /* eslint-disable vars-on-top */
 
+// Import * as schema from './schema';
+// Import { drizzle } from 'drizzle-orm/postgres-js';
+// Import postgres from 'postgres';
+
+// Const connectionString = process.env.DATABASE_URL;
+
+// // Disable prefetch as it is not supported for "Transaction" pool mode
+// Export const client = postgres(connectionString!, { prepare: false });
+// Export const db = drizzle(client, { schema });
+
 import * as schema from './schema';
 import { PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
