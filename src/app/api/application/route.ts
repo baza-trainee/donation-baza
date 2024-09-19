2220a791cddce9df48018e676cda3fd340a5e983
cold <kholodihor@gmail.com>
2024-09-19 12:44:31 +0300

    fix: change orm to prisma

--- src/app/api/application/route.ts
+++ src/app/api/application/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
-import { db } from '@/lib/db/db';
+import prisma from '@/lib/prisma';
 
 export async function GET() {
   try {
-    const applications = await db.query.applications.findMany();
+    const applications = await prisma.application.findMany();
     return NextResponse.json(applications, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: error }, { status: 500 });
--- src/lib/db/schema.ts
+++
@@ -1,3 +0,0 @@
-export { default as users } from './schema/user';
-export { default as session } from './schema/session';
-export { default as applications } from './schema/application';
