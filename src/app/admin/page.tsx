4328c3f134d07b1402fe0a74c7e28031203f3173
cold <kholodihor@gmail.com>
2024-09-20 09:14:17 +0300

    refactor: refactor auth components

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,21 +1,17 @@
-'use client';
-
-'use client';
-
 import SignOutButton from '@/components/ui/SignOutButton';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+import { getAuthSession } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
-const page = () => {
-  const session = useSession();
-  const router = useRouter();
+const page = async () => {
+  const session = await getAuthSession();
 
-  if (!session?.data?.user) {
-    router.replace('/login');
+  if (!session) {
+    redirect('/login');
   }
+
   return (
     <div className="page_wrapper">
-      User {session?.data?.user?.email} signed in
+      User {session?.user?.email} signed in
       <SignOutButton />
     </div>
   );
