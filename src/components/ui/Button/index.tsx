29e419cc4a86b586a3a7b9ebcc477c047b74ed42
Maxym Biriukov <biryukov.mm@gmail.com>
2024-09-23 15:04:01 +0300

    feat: create button component template

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary' | 'outline';
+interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'outline' | 'underline' | 'icon';
+  size?: 'small' | 'medium' | 'large';
+  icon?: 'uah' | 'zloty' | 'eur' | React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({
+const Button: React.FC<IButton> = ({
   children,
   variant = 'primary',
+  size = 'medium',
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  icon,
   className,
   ...props
 }) => {
   return (
     <button
-      className={`${styles.button} ${styles[variant]} ${className}`}
+      className={`${styles.container} ${styles[variant]} ${styles[size]} ${className}`}
       {...props}
     >
-      {children}
+      {variant === 'icon' ? (
+        <div className={styles.flex}>{children}</div>
+      ) : (
+        children
+      )}
     </button>
   );
 };
