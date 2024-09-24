a77c85995b6f6f01ab1884607c224e22a027a147
oSlavivna <hotyninfo@gmail.com>
2024-09-24 14:57:08 +0300

    fix: footer changes

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -15,12 +15,7 @@ const Footer: React.FC = () => {
       <div className={styles.footerTop}>
         {/* Logo */}
         <div className={styles.logo}>
-          <Image
-            src="/img/footer/logo-footer.png"
-            alt="Логотип"
-            width={100}
-            height={100}
-          />
+          <Image src="/svg/logo.svg" alt="Логотип" width={100} height={100} />
         </div>
         {/* About us */}
         <div className={styles.footerMenu}>
