fd452e36b8cfd4e0daad8d142ca32cd0d8be6d7d
Maxym Biriukov <biryukov.mm@gmail.com>
2024-09-27 03:54:33 +0300

    refactor: add dynamic animation duration to ensure constant animation speed

--- src/components/common/Banner/Banner.tsx
+++ src/components/common/Banner/Banner.tsx
@@ -1,10 +1,23 @@
+'use client';
+import { useEffect, useRef } from 'react';
 import Button from '@/components/ui/Button';
 import Image from 'next/image';
-import React from 'react';
 import styles from './Banner.module.scss';
 
 const Banner: React.FC = () => {
   const banner = 'Військовий збір для 44 бригади!';
+  const containerRef = useRef<HTMLDivElement>(null);
+  const textRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current && textRef.current) {
+      const textWidth = textRef.current.offsetWidth;
+      const speed = 100;
+      const duration = textWidth / speed;
+      containerRef.current.style.setProperty('--text-width', `${textWidth}px`);
+      containerRef.current.style.setProperty('--duration', `${duration}s`);
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -17,9 +30,9 @@ const Banner: React.FC = () => {
         />
       </div>
 
-      <div className={styles.collection}>
-        <div className={styles.text1}>
-          {Array(6)
+      <div className={styles.collection} ref={containerRef}>
+        <div className={styles.text1} ref={textRef}>
+          {Array(5)
             .fill(null)
             .map((currentValue, index) => (
               <span key={index}>{banner}</span>
