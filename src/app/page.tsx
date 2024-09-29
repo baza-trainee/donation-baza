4de30484dde779194e9e8b729cab38052c8d5e23
Maxym Biriukov <biryukov.mm@gmail.com>
2024-09-29 21:48:51 +0300

    feat: delete banner from common components; connected bannersection to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Banner from '@/components/common/Banner/Banner';
+import BannerSection from '@/features/home/sections/BannerSection';
 import ButtonsExample from '@/components/examples/ButtonsExample';
 import ContactUsFormSection from '@/features/home/sections/ContactUsFormSection';
 import ErrorMessage from '@/components/common/ErrorMessage/ErrorMessage';
@@ -8,7 +8,7 @@ import TextExample from '@/components/examples/TextExample';
 export default function Home() {
   return (
     <>
-      <Banner />
+      <BannerSection />
       <ContactUsFormSection />
       <Modal modalKey="error_message">
         <ErrorMessage />
--- src/features/home/sections/BannerSection/index.tsx
+++ src/features/home/sections/BannerSection/index.tsx
@@ -1,31 +1,28 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import Button from '@/components/ui/Button';
 import Image from 'next/image';
 import styles from './BannerSection.module.scss';
 
-const Banner: React.FC = () => {
-  const banner = ' Військовий збір для 44 бригади! ';
-
+const BannerSection: React.FC = () => {
+  const banner = 'Військовий збір для 44 бригади!';
   const containerRef = useRef<HTMLDivElement>(null);
   const textRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (containerRef.current && textRef.current) {
       const textWidth = textRef.current.offsetWidth;
-      const containerWidth = containerRef.current.offsetWidth;
-
+      const containerWidth = containerRef.current.clientWidth;
       const speed = 100;
       const duration = (textWidth - containerWidth) / speed;
       containerRef.current.style.setProperty(
         '--text-width',
         `${textWidth - containerWidth}px`
       );
-
       containerRef.current.style.setProperty('--duration', `${duration}s`);
     }
   }, []);
-  //
+
   return (
     <div className={styles.container}>
       <div className={styles.iconBell}>
@@ -39,9 +36,11 @@ const Banner: React.FC = () => {
 
       <div className={styles.collection} ref={containerRef}>
         <div className={styles.text} ref={textRef}>
-          {/* {scrollingText} */}
-          {banner}
-          {banner}
+          {Array(15)
+            .fill(banner)
+            .map((text, index) => (
+              <span key={index}>{text}</span>
+            ))}
         </div>
       </div>
 
@@ -58,4 +57,4 @@ const Banner: React.FC = () => {
   );
 };
 
-export default Banner;
+export default BannerSection;
