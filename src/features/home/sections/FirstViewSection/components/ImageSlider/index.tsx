e6a4e48e168bc94cd331fa94514cd3f9161c0c91
oSlavivna <hotyninfo@gmail.com>
2024-10-15 19:56:16 +0300

    fix: slider shrinks too much

--- src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
+++ src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
@@ -4,21 +4,19 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import React from 'react';
 import Slider from 'react-slick';
-// // не знаю чи це потрібно
-// І це теж
 import styles from './ImageSlider.module.scss';
 
 const ImageSlider: React.FC = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 3000,
     cssEase: 'linear',
-    arrows: false,
+    // Arrows: true,
   };
 
   const images = [
