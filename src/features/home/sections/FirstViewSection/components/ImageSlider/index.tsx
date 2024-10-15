0b8b54dcb74a89603f373eb3aceed35563f2b331
O_Slavivna <133753511+oSlavivna@users.noreply.github.com>
2024-10-15 19:37:17 +0300

    Update index.tsx

--- src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
+++ src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
@@ -4,8 +4,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import React from 'react';
 import Slider from 'react-slick';
-// // не знаю чи це потрібно
-// І це теж
 import styles from './ImageSlider.module.scss';
 
 const ImageSlider: React.FC = () => {
