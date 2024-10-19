7e623ab582e59b82840989e9e9556c86e2993d3f
cold <kholodihor@gmail.com>
2024-10-19 16:19:44 +0300

    fix: remove backend

---
+++ src/constants/documents.ts
@@ -0,0 +1,18 @@
+export const documents = [
+  {
+    key: 'privacy_policy',
+    url: '/docs/Політика конфіденційності.pdf',
+  },
+  {
+    key: 'terms_of_service',
+    url: '/docs/Ппавилв користування сайтом.pdf',
+  },
+  {
+    key: 'accounting',
+    url: '/docs/Політика конфіденційності.pdf',
+  },
+  {
+    key: 'regulations',
+    url: '/docs/Політика конфіденційності.pdf',
+  },
+];
--- src/utils/api/events.ts
+++
@@ -1,7 +0,0 @@
-import { IEvent } from '@/types/common.types';
-import axios from 'axios';
-
-export const getContacts = async () => {
-  const { data } = await axios.get<IEvent[]>('/api/events');
-  return data;
-};
