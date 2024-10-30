36fdc404e350d46533f8115701a14da8047ec97e
Maxym Biriukov <biryukov.mm@gmail.com>
2024-10-30 15:17:38 +0200

    feat: connected pdf viewer to footer links; styled pdf viewer

--- src/components/common/Footer/components/FooterDocuments/index.tsx
+++ src/components/common/Footer/components/FooterDocuments/index.tsx
@@ -1,20 +1,33 @@
-import Link from 'next/link';
+import Modal from '@/components/common/Modal/Modal';
+import { MouseEvent } from 'react';
+import PDFViewer from '@/components/common/PDFViewer/PDFViewer';
 import { documents } from '@/constants/documents';
 import styles from './FooterDocuments.module.scss';
+import { useModalContext } from '@/context/ModalContext';
 import { useTranslations } from 'next-intl';
 
 export const FooterDocuments: React.FC = () => {
   const translate = useTranslations('common');
-
+  const { openModal } = useModalContext();
+  const handleDocumentClick = (
+    event: MouseEvent<HTMLSpanElement, globalThis.MouseEvent>,
+    key: string
+  ) => {
+    event.stopPropagation();
+    openModal(key);
+  };
   return (
     <div className={styles.wrapper}>
       <ul>
         {documents &&
           documents.map((doc) => (
             <li key={doc.key}>
-              <Link href={doc.url} target="_blank">
+              <span onClick={(event) => handleDocumentClick(event, doc.key)}>
                 {translate(`docs.${doc.key}`)}
-              </Link>
+              </span>
+              <Modal modalKey={doc.key}>
+                <PDFViewer file={doc.url} />
+              </Modal>
             </li>
           ))}
       </ul>
--- src/components/common/PDFViewer/PDFViewer.tsx
+++ src/components/common/PDFViewer/PDFViewer.tsx
@@ -2,7 +2,8 @@
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import styles from './PDFViewer.module.scss';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
@@ -13,26 +14,54 @@ interface PDFViewerProps {
 const PDFViewer: React.FC<PDFViewerProps> = ({ file }) => {
   const [pages, setPages] = useState<number>();
   const [pageNumber, setPageNumber] = useState<number>(1);
+  const pageRef = useRef<HTMLDivElement>(null);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setPages(numPages);
   }
 
+  const changePage = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+    if (pageRef.current) {
+      pageRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  useEffect(() => {
+    const preventCopy = (ev: Event) => {
+      ev.preventDefault();
+    };
+
+    document.addEventListener('copy', preventCopy);
+    document.addEventListener('cut', preventCopy);
+    document.addEventListener('contextmenu', preventCopy);
+
+    return () => {
+      document.removeEventListener('copy', preventCopy);
+      document.removeEventListener('cut', preventCopy);
+      document.removeEventListener('contextmenu', preventCopy);
+    };
+  }, []);
+
   return (
     <div>
-      <Document {...{ file }} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {pages}
-      </p>
-      <div>
-        {pageNumber > 1 && (
-          <button onClick={() => setPageNumber(pageNumber - 1)}>←</button>
-        )}
-        {pages && pageNumber + 1 <= pages && (
-          <button onClick={() => setPageNumber(pageNumber + 1)}>→</button>
-        )}
+      <div className={styles.wrapper}>
+        <article ref={pageRef}>
+          <Document {...{ file }} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </article>
+        <div className={styles.pageIndicator}>
+          Page {pageNumber} of {pages}
+        </div>
+        <div className={styles.pagination}>
+          {pageNumber > 1 && (
+            <button onClick={() => changePage(pageNumber - 1)}>←</button>
+          )}
+          {pages && pageNumber + 1 <= pages && (
+            <button onClick={() => changePage(pageNumber + 1)}>→</button>
+          )}
+        </div>
       </div>
     </div>
   );
