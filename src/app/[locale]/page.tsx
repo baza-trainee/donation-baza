52662e966da19cfc4dc66d5718728c321a931a5b
Maxym Biriukov <biryukov.mm@gmail.com>
2024-10-30 15:16:27 +0200

    chore: remove unnecessary code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,18 +6,15 @@ import EventsSection from '@/features/home/sections/EventsSection';
 import FirstViewSection from '@/features/home/sections/FirstViewSection';
 import HelpNowSection from '@/features/home/sections/HelpNowSection';
 import Modal from '@/components/common/Modal/Modal';
-import PDFViewer from '@/components/common/PDFViewer/PDFViewer';
 import QASection from '@/features/home/sections/QASection';
 import SecondViewSection from '@/features/home/sections/SecondViewSection';
 import TextExample from '@/components/examples/TextExample';
-import { documents } from '@/constants/documents';
 
 export default function Home() {
   return (
     <>
       <FirstViewSection />
       <BannerSection />
-      <PDFViewer file={documents[0].url} />
       <SecondViewSection />
       <QASection />
       <HelpNowSection />
