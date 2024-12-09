eaa6d6c8ad42cee5ed8c950d8eefc354466d07fc
Ihor Kholod <87018852+kholodihor@users.noreply.github.com>
2024-12-09 13:06:13 +0200

    fix: fix validation in application form (#67)

--- src/features/home/sections/ContactUsFormSection/components/ApplicationForm/applicationScheme.ts
+++ src/features/home/sections/ContactUsFormSection/components/ApplicationForm/applicationScheme.ts
@@ -1,4 +1,5 @@
-import { emailPattern, restrictedDomens } from '@/constants/regEx';
+/* eslint-disable require-unicode-regexp */
+import { emailPattern, restrictedDomains } from '@/constants/regEx';
 import { z } from 'zod';
 
 export const applicationScheme = z.object({
@@ -6,18 +7,27 @@ export const applicationScheme = z.object({
     .string()
     .min(1, 'Введіть ім’я')
     .min(2, 'Ім’я повинно мати не менше 2 знаків')
-    .max(30, 'Ім’я повинно бути не більше 30 знаків'),
+    .max(30, 'Ім’я повинно бути не більше 30 знаків')
+    .regex(/^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ’'-\s]+$/, {
+      message: 'Ім’я може містити лише дозволені символи',
+    })
+    .refine((value) => value.trim().length > 0, {
+      message: 'Ім’я не може складатися лише з пробілів',
+    }),
   email: z
     .string()
     .min(1, 'Введіть email')
     .regex(emailPattern, {
       message: 'Введіть дійсний email',
     })
-    .refine((value) => !restrictedDomens.test(value.split('@')[1]), {
+    .refine((value) => !restrictedDomains.test(value.split('@')[1]), {
       message: 'Домени .ru і .by не дозволені',
     }),
   message: z
     .string()
     .min(1, 'Введіть повідомлення')
-    .max(300, 'Просимо скоротити ваше повідомлення до 300 знаків'),
+    .max(300, 'Просимо скоротити ваше повідомлення до 300 знаків')
+    .refine((value) => value.trim().length > 0, {
+      message: 'Повідомлення не може бути пустим',
+    }),
 });
