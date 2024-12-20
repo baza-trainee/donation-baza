b5a7919b466d3cbe5d309773e569aeba3fd988f0
Olya <ponomaryova.olya.y@gmail.com>
2024-12-20 18:34:02 +0100

    refactor: improve code for readability; add card title according to figma for specific screens

--- src/features/home/sections/EventsSection/components/Event/EventCard.tsx
+++ src/features/home/sections/EventsSection/components/Event/EventCard.tsx
@@ -18,55 +18,68 @@ const EventCard: React.FC<EventCardProps> = ({
   buttonText,
 }) => {
   const [isOpened, setIsOpened] = useState(false);
-  return isOpened ? (
-    <article className={`${styles.wrapper} ${styles.wrapperOpened}`}>
-      <div className={`${styles.description} ${styles.descriptionOpened}`}>
-        <button
-          className={styles.backBtn}
-          type="button"
-          onClick={() => setIsOpened((prevState) => !prevState)}
-        >
-          <ArrowLeftWithoutDash />
-        </button>
-        <div className={styles.textContainer}>
-          <p className={`${styles.text} ${styles.opened}`}>{description}</p>
-        </div>
-      </div>
-    </article>
-  ) : (
-    <article className={styles.wrapper}>
-      <div className={styles.imageContainer}>
-        <Image
-          style={{ objectFit: 'cover' }}
-          quality={90}
-          loading="lazy"
-          src={image}
-          width={768}
-          height={814}
-          alt={imageAlt}
-        />
-      </div>
 
-      <div className={styles.description}>
-        <div className={styles.textContainer}>
-          <header>
-            <h2>{title}</h2>
-          </header>
-          <p className={`${styles.text} ${styles.closed}`}>{description}</p>
+  const renderOpened = () => {
+    return (
+      <article className={`${styles.wrapper} ${styles.wrapperOpened}`}>
+        <div className={`${styles.description} ${styles.descriptionOpened}`}>
+          <div className={styles.textContainer}>
+            <div className={styles.titleWrapper}>
+              <button
+                className={styles.backBtn}
+                type="button"
+                onClick={() => setIsOpened((prevState) => !prevState)}
+              >
+                <ArrowLeftWithoutDash />
+              </button>
+              <header className={`${styles.title} ${styles.textContainer}`}>
+                <h2>{title}</h2>
+              </header>
+            </div>
+            <p className={`${styles.text} ${styles.opened}`}>{description}</p>
+          </div>
         </div>
-        <Button
-          variant="underline"
-          size="small"
-          type="button"
-          onClick={() => setIsOpened((prevState) => !prevState)}
-          aria-label="Читати далі"
-          className={styles.readMoreBtn}
-        >
-          {buttonText}
-        </Button>
-      </div>
-    </article>
-  );
+      </article>
+    );
+  };
+
+  const renderClosed = () => {
+    return (
+      <article className={styles.wrapper}>
+        <div className={styles.imageContainer}>
+          <Image
+            style={{ objectFit: 'cover' }}
+            quality={90}
+            loading="lazy"
+            src={image}
+            width={768}
+            height={814}
+            alt={imageAlt}
+          />
+        </div>
+        <div className={styles.description}>
+          <div className={styles.textContainer}>
+            <header>
+              <h2>{title}</h2>
+            </header>
+            <p className={`${styles.text} ${styles.closed}`}>{description}</p>
+          </div>
+          <Button
+            variant="underline"
+            size="small"
+            type="button"
+            onClick={() => setIsOpened((prevState) => !prevState)}
+            aria-label="Читати далі"
+            className={styles.readMoreBtn}
+          >
+            {buttonText}
+          </Button>
+        </div>
+      </article>
+    );
+  };
+
+  return isOpened ? renderOpened() : renderClosed();
 };
 
 export default EventCard;
