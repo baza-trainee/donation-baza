bab1b3f22385e48ac3082dbaf7fc5d46509c68c0
O_Slavivna <133753511+oSlavivna@users.noreply.github.com>
2024-12-27 20:27:16 +0200

    feat: add arrows (#70)
    * feat: add arrows
    
    * chore: replaced photo images to jpg; 
    
    * chore: use local imports
    
    * chore: add blur to slider image on loading
    
    * refactor: hide arrows on lower than desktop resolution
    
    ---------
    
    Co-authored-by: Maxym Biriukov <biryukov.mm@gmail.com>

--- src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
+++ src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
@@ -1,7 +1,12 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { SampleNextArrow, SamplePrevArrow } from './CustomArrows/CustomArrows';
+import Image from 'next/image';
 import React from 'react';
 import Slider from 'react-slick';
+import slide00 from '/public/img/hero/slide-00.jpg';
+import slide01 from '/public/img/hero/slide-01.jpg';
+import slide02 from '/public/img/hero/slide-02.jpg';
 import styles from './ImageSlider.module.scss';
 
 interface ImageSliderProps {
@@ -10,7 +15,6 @@ interface ImageSliderProps {
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ onSlideChange }) => {
   const settings = {
-    dots: true,
     infinite: true,
     speed: 600,
     slidesToShow: 1,
@@ -18,22 +22,19 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ onSlideChange }) => {
     autoplay: true,
     autoplaySpeed: 3000,
     cssEase: 'linear',
-    Arrows: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
     beforeChange: (current: number, next: number) => onSlideChange(next),
   };
 
-  const images = [
-    '/img/hero/mainPhoto.png',
-    '/img/hero/test2.png',
-    '/img/hero/hero-test-img.png',
-  ];
+  const images = [slide00, slide01, slide02];
 
   return (
     <div className={styles.sliderContainer}>
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index}>
-            <img src={image} alt={`Image ${index}`} />
+            <Image src={image} alt={`Image ${index}`} placeholder="blur" />
           </div>
         ))}
       </Slider>
