cbcbbbca8944b566dc4241b43ef873df65e48801
Maxym Biriukov <biryukov.mm@gmail.com>
2024-12-27 16:45:37 +0200

    chore: use local imports

--- src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
+++ src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
@@ -4,7 +4,11 @@ import { SampleNextArrow, SamplePrevArrow } from './CustomArrows/CustomArrows';
 import Image from 'next/image';
 import React from 'react';
 import Slider from 'react-slick';
+import slide00 from '/public/img/hero/slide-00.jpg';
+import slide01 from '/public/img/hero/slide-01.jpg';
+import slide02 from '/public/img/hero/slide-02.jpg';
 import styles from './ImageSlider.module.scss';
+
 interface ImageSliderProps {
   onSlideChange: (index: number) => void;
 }
@@ -23,26 +27,14 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ onSlideChange }) => {
     beforeChange: (current: number, next: number) => onSlideChange(next),
   };
 
-  const images = [
-    '/img/hero/slide-00.jpg',
-    '/img/hero/slide-01.jpg',
-    '/img/hero/slide-02.jpg',
-  ];
+  const images = [slide00, slide01, slide02];
 
   return (
     <div className={styles.sliderContainer}>
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index}>
-            <Image
-              src={image}
-              alt={`Image ${index}`}
-              width={1920}
-              height={1133}
-              layout="responsive"
-              objectFit="cover"
-              className={styles.sliderImage}
-            />
+            <Image src={image} alt={`Image ${index}`} objectFit="cover" />
           </div>
         ))}
       </Slider>
