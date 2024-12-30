6e6409e52431c8aea2bf7dfc62b6e45228e109db
Maxym Biriukov <biryukov.mm@gmail.com>
2024-12-30 13:50:35 +0200

    feat: add translation

--- src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
+++ src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
@@ -1,6 +1,6 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { SampleNextArrow, SamplePrevArrow } from './CustomArrows/CustomArrows';
+import { SampleNextArrow, SamplePrevArrow } from '../CustomArrows/CustomArrows';
 import Image from 'next/image';
 import React from 'react';
 import Slider from 'react-slick';
--- src/features/home/sections/FirstViewSection/index.tsx
+++ src/features/home/sections/FirstViewSection/index.tsx
@@ -1,30 +1,26 @@
 'use client';
 import ImageSlider from '@/features/home/sections/FirstViewSection/components/ImageSlider';
+import SlideDescription from './components/SlideDescription';
 import styles from './FirstViewSection.module.scss';
 import { useState } from 'react';
+import { useTranslations } from 'next-intl';
 
 const FirstViewSection: React.FC = () => {
-  const [author, setAuthor] = useState('© Анастасія Сусло, 2024');
   const authors = [
     '© Анастасія Сусло, 2024',
     '© Анастасія Сусло, 2024',
     '© Олена Ільєнко, 2024',
   ];
-
+  const [author, setAuthor] = useState(authors[0]);
+  const translation = useTranslations('homepage.firstViewSection');
+  const header = translation('header');
+  const subheader = translation('subheader');
+  const textTop = translation('textTop');
+  const textBottom = translation('textBottom');
   return (
     <section className={styles.firstViewSection}>
-      <ImageSlider
-        onSlideChange={(index: number) => setAuthor(authors[index])}
-      />
-      <div className={styles.wrapper}>
-        <h1 className={styles.header}>Культура благочинності -</h1>
-        <h2 className={styles.aboveHeader}>це не про внески і донати.</h2>
-        <p className={styles.text}>
-          Це історія про спільноту, де кожен почувається сильним або
-          підтримуваним, де кожен може захистити або бути захищеним.
-        </p>
-        <p className={styles.text}>Це історія про українців.</p>
-      </div>
+      <ImageSlider onSlideChange={(idx) => setAuthor(authors[idx])} />
+      <SlideDescription {...{ header, subheader, textTop, textBottom }} />
       <div className={styles.photoAutor}>
         <p className={styles.autor}>{author}</p>
       </div>
