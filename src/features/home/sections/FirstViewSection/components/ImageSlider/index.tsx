d882c985f14c3271947e56172900562c6b3140ef
Maxym Biriukov <biryukov.mm@gmail.com>
2025-01-06 19:52:17 +0200

    feat: restyle image slider; add mobile images

--- src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
+++ src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
@@ -5,8 +5,11 @@ import Image from 'next/image';
 import React from 'react';
 import Slider from 'react-slick';
 import slide00 from '/public/img/hero/slide-00.jpg';
+import slide00Small from '/public/img/hero/slide-00-420px.jpg';
 import slide01 from '/public/img/hero/slide-01.jpg';
+import slide01Small from '/public/img/hero/slide-01-420px.jpg';
 import slide02 from '/public/img/hero/slide-02.jpg';
+import slide02Small from '/public/img/hero/slide-02-420px.jpg';
 import styles from './ImageSlider.module.scss';
 
 interface ImageSliderProps {
@@ -19,7 +22,8 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ onSlideChange }) => {
     speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
+    pauseonhover: true,
     autoplaySpeed: 3000,
     cssEase: 'linear',
     nextArrow: <SampleNextArrow />,
@@ -27,14 +31,29 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ onSlideChange }) => {
     beforeChange: (current: number, next: number) => onSlideChange(next),
   };
 
-  const images = [slide00, slide01, slide02];
+  const images = [
+    { large: slide00, small: slide00Small },
+    { large: slide01, small: slide01Small },
+    { large: slide02, small: slide02Small },
+  ];
 
   return (
     <div className={styles.sliderContainer}>
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index}>
-            <Image src={image} alt={`Image ${index}`} placeholder="blur" />
+            <Image
+              src={image.large}
+              alt={`Image ${index}`}
+              placeholder="blur"
+              className={styles.image}
+            />
+            <Image
+              src={image.small}
+              alt={`Image ${index}`}
+              placeholder="blur"
+              className={styles.imageSmall}
+            />
           </div>
         ))}
       </Slider>
