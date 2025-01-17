f0319dc668cc4057149674ae4dbc94777204ff61
Maxym Biriukov <biryukov.mm@gmail.com>
2025-01-17 20:04:45 +0200

    feat: translate

--- src/features/home/sections/SecondViewSection/index.tsx
+++ src/features/home/sections/SecondViewSection/index.tsx
@@ -2,42 +2,34 @@ import Button from '@/components/ui/Button';
 import Image from 'next/image';
 import Picture from '/public/img/support-test-img.png';
 import styles from './SecondViewSection.module.scss';
+import { useTranslations } from 'next-intl';
 
 const SecondViewSection = () => {
+  const translate = useTranslations('homepage.secondViewSection');
   return (
     <section className={styles.container}>
       <article className={styles.article}>
-        <header className={styles.header}>Важливість підтримки</header>
+        <header className={styles.header}>{translate('header')}</header>
         <div className={styles.text}>
-          <p>
-            Ознака розвиненого суспільства — благодійність приватна, локальна,
-            корпоративна або на рівні держави.
-          </p>
-          <p>
-            Прозорість, долученість, контроль — ось три стовпи сучасного
-            благодійництва.
-          </p>
-          <p>
-            Постійні найменші внески гуртують спільноту, підтримують вразливі
-            категорії, а також контролюють отримувачів допомоги.
-          </p>
+          <p>{translate('firstParagraph')}</p>
+          <p>{translate('secondParagraph')}</p>
+          <p>{translate('thirdParagraph')} </p>
         </div>
         <Button variant="underline" size="large" className={styles.button}>
-          Допомогти Baza
+          {translate('button')}
         </Button>
       </article>
 
       <div className={styles.pictureСontainer}>
-        <h3 className={styles.preheader}>Важливість підтримки</h3>
+        <h3 className={styles.preheader}>{translate('header')}</h3>
         <Image
           src={Picture}
-          alt="Важливість підтримки"
+          alt={translate('header')}
           className={styles.image}
         />
-        <p className={styles.author}>©Марина Федоренко, 2024</p>
+        <p className={styles.author}>{translate('copyright')}</p>
       </div>
     </section>
   );
 };
-
 export default SecondViewSection;
