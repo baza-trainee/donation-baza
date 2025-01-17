2320c82d36733ce6a28e9ba0ed0143b73d8fe615
Biriukov Maxym <biryukov.mm@gmail.com>
2025-01-17 21:17:25 +0200

    Feature/make slider responsive (#75)
    * chore: remove unnecessary files
    
    * feat: add translation
    
    * chore: remove unnecessary props in custom arrows
    
    * feat: add 1280 breakpoint
    
    * feat: restyle image slider; add mobile images
    
    * feat: slide description styling fix from desktop to tabled bps
    
    * feat: tablet and mobile description styling
    
    * chore: revert temporary slide settings changes
    
    * chore: add translations for img authors
    
    * chore: remove unnecessary
    
    * chore: change arrows breakpoints
    
    * chore: remove unready sections from main page
    
    * fix: hotfix banner section
    
    ---------
    
    Co-authored-by: Olya <ponomaryova.olya.y@gmail.com>

---
+++ src/features/home/sections/FirstViewSection/components/SlideDescription/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './SlideDescription.module.scss';
+
+interface IProps {
+  header: string;
+  subheader: string;
+  textTop: string;
+  textBottom: string;
+}
+
+const SlideDescription: React.FC<IProps> = ({
+  header,
+  subheader,
+  textTop,
+  textBottom,
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.header}>{header}</h1>
+      <h2 className={styles.subheader}>{subheader}</h2>
+      <p className={styles.text}>{textTop}</p>
+      <p className={styles.text}>{textBottom}</p>
+    </div>
+  );
+};
+
+export default SlideDescription;
