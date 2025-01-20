fecc7da921b447aaad77a9797835d5c11a394670
Maxym Biriukov <biryukov.mm@gmail.com>
2025-01-20 15:22:38 +0200

    feat: add dynamic background

--- src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
+++ src/features/home/sections/FirstViewSection/components/ImageSlider/index.tsx
@@ -3,6 +3,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import { SampleNextArrow, SamplePrevArrow } from '../CustomArrows/CustomArrows';
 import Image from 'next/image';
 import React from 'react';
+import SlideDescription from '../SlideDescription';
 import Slider from 'react-slick';
 import slide00 from '/public/img/hero/slide-00.jpg';
 import slide00Small from '/public/img/hero/slide-00-420px.jpg';
@@ -11,6 +12,7 @@ import slide01Small from '/public/img/hero/slide-01-420px.jpg';
 import slide02 from '/public/img/hero/slide-02.jpg';
 import slide02Small from '/public/img/hero/slide-02-420px.jpg';
 import styles from './ImageSlider.module.scss';
+import { useTranslations } from 'next-intl';
 
 interface ImageSliderProps {
   onSlideChange: (index: number) => void;
@@ -37,11 +39,20 @@ const ImageSlider: React.FC<ImageSliderProps> = ({ onSlideChange }) => {
     { large: slide02, small: slide02Small },
   ];
 
+  const translation = useTranslations('homepage.firstViewSection');
+  const header = translation('header');
+  const subheader = translation('subheader');
+  const textTop = translation('textTop');
+  const textBottom = translation('textBottom');
+
   return (
     <div className={styles.sliderContainer}>
       <Slider {...settings}>
         {images.map((image, index) => (
           <div key={index}>
+            <SlideDescription
+              {...{ header, subheader, textTop, textBottom, index }}
+            />
             <Image
               src={image.large}
               alt={`Image ${index}`}
--- src/features/home/sections/FirstViewSection/index.tsx
+++ src/features/home/sections/FirstViewSection/index.tsx
@@ -1,16 +1,12 @@
 'use client';
 import ImageSlider from '@/features/home/sections/FirstViewSection/components/ImageSlider';
-import SlideDescription from './components/SlideDescription';
 import styles from './FirstViewSection.module.scss';
 import { useState } from 'react';
 import { useTranslations } from 'next-intl';
 
 const FirstViewSection: React.FC = () => {
   const translation = useTranslations('homepage.firstViewSection');
-  const header = translation('header');
-  const subheader = translation('subheader');
-  const textTop = translation('textTop');
-  const textBottom = translation('textBottom');
+
   const authors = [
     translation('imgAuthor-1'),
     translation('imgAuthor-1'),
@@ -21,7 +17,6 @@ const FirstViewSection: React.FC = () => {
   return (
     <section className={styles.firstViewSection}>
       <ImageSlider onSlideChange={(idx) => setAuthor(authors[idx])} />
-      <SlideDescription {...{ header, subheader, textTop, textBottom }} />
       <div className={styles.photoAutor}>
         <p className={styles.autor}>{author}</p>
       </div>
