56c421008525cabeb7c9cff06a5f173d55b94cbb
Biriukov Maxym <biryukov.mm@gmail.com>
2025-01-25 23:45:24 +0200

    Bugfix/application form (modal form responsive) (#85)
    * feat: translate application form
    
    * feat: make modal responsive

--- src/features/home/sections/ContactUsFormSection/components/ApplicationFeedback/ApplicationFeedback.tsx
+++ src/features/home/sections/ContactUsFormSection/components/ApplicationFeedback/ApplicationFeedback.tsx
@@ -1,12 +1,12 @@
 import styles from './ApplicationFeedback.module.scss';
+import { useTranslations } from 'next-intl';
 
 const ApplicationFeedback = () => {
+  const translate = useTranslations('common.modal.applicationFeedback');
   return (
     <div className={styles.feedbackLayout}>
-      <h2 className={styles.feedbackTitle}>Дякуємо за звернення.</h2>
-      <p className={styles.feedbackText}>
-        Ваша заявка буде розглянута протягом 24 годин.
-      </p>
+      <h2 className={styles.feedbackTitle}>{translate('header')}</h2>
+      <p className={styles.feedbackText}>{translate('text')}</p>
     </div>
   );
 };
