ec728859cb7d8fed54c6d0035b09e81dcb96e15b
oSlavivna <hotyninfo@gmail.com>
2025-01-29 16:08:15 +0200

    feat: add responsive without 420

--- src/app/[locale]/safety/page.tsx
+++ src/app/[locale]/safety/page.tsx
@@ -5,54 +5,67 @@ import SecurityCard from './SecurityCard/SecurityCard';
 import styles from './safety.module.scss';
 
 const SecurityPage: React.FC = () => {
+  const cards = [
+    {
+      icon: '/svg/customer-satisfaction.svg',
+      description: 'Використовує емоційність в описі проблематики',
+    },
+    {
+      icon: '/svg/tribune.svg',
+      description:
+        'Використовує складні технічні чи медичні терміни при описі збору в надії, що того ніхто не буде перевіряти',
+    },
+    {
+      icon: '/svg/wanted.svg',
+      description: "Відсутня конкретна звітність на об'явлений збір",
+    },
+    {
+      icon: '/svg/notes.svg',
+      description:
+        'Постійно додається детальна інформація про зміни в процесах',
+    },
+    {
+      icon: '/svg/document.svg',
+      description: 'Блокує у відповідь на уточнюючі питання',
+    },
+    {
+      icon: '/svg/anonymous-man.svg',
+      description:
+        'Не відповідає на прямі питання, що вимагають відповіді Так чи Ні',
+    },
+    {
+      icon: '/svg/consulting.svg',
+      description:
+        'Спамить проханнями персонального характеру, не будучи знайомим близько',
+    },
+    {
+      icon: '/svg/women.svg',
+      description:
+        'Не погоджується на пропозицію замість грошей надіслати конкретні речі',
+    },
+    {
+      icon: '/svg/guard.svg',
+      description:
+        'В якості звітності надає не чеки, а фото заявлених предметів',
+    },
+    {
+      icon: '/svg/nophone.svg',
+      description:
+        'Дуже наполегливий в своїй діяльності, імітує активність до останнього',
+    },
+  ];
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h2>Як відрізнити шахрая від волонтера:</h2>
         <ul>
-          <SecurityCard
-            icon="/svg/customer-satisfaction.svg"
-            description="Використовує емоційність в описі проблематики"
-          />
-          <SecurityCard
-            icon="/svg/tribune.svg"
-            description="Використовує складні технічні чи медичні терміни при описі збору
-                в надії, що того ніхто не буде перевіряти"
-          />
-          <SecurityCard
-            icon="/svg/wanted.svg"
-            description="Відсутня конкретна звітність на об'явлений збір"
-          />
-          <SecurityCard
-            icon="/svg/notes.svg"
-            description="Постійно додається детальна інформація про зміни в процесах"
-          />
-          <SecurityCard
-            icon="/svg/document.svg"
-            description="Блокує у відповідь на уточнюючі питання"
-          />
-          <SecurityCard
-            icon="/svg/anonymous-man.svg"
-            description="Не відповідає на прямі питання, що вимагають відповіді Так чи Ні"
-          />
-          <SecurityCard
-            icon="/svg/consulting.svg"
-            description="Спамить проханнями персонального характеру, не будучи знайомим
-                близько"
-          />
-          <SecurityCard
-            icon="/svg/women.svg"
-            description="Не погоджується на пропозицію замість грошей надіслати конкретні
-                речі"
-          />
-          <SecurityCard
-            icon="/svg/guard.svg"
-            description="В якості звітності надає не чеки, а фото заявлених предметів"
-          />
-          <SecurityCard
-            icon="/svg/nophone.svg"
-            description="Дуже наполегливий в своїй діяльності, імітує активність до останнього"
-          />
+          {cards.map((card, index) => (
+            <SecurityCard
+              key={index}
+              icon={card.icon}
+              description={card.description}
+            />
+          ))}
         </ul>
       </div>
 
