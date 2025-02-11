dbd1be715c39ebfbe7ebffc6e39fa7ca6afa4b64
Maxym Biriukov <biryukov.mm@gmail.com>
2025-02-11 13:12:48 +0200

    feat: enhance modal and PDF viewer components with new layouts and styles

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -17,7 +17,6 @@ const Modal = ({ modalKey, children }: ModalProps) => {
   useBodyScrollLock(modals[modalKey]);
 
   const isModalOpen = modals[modalKey];
-
   const ModalLayout = () => (
     <div className={styles.modalOverlay}>
       <div className={styles.modalContent}>
@@ -29,7 +28,27 @@ const Modal = ({ modalKey, children }: ModalProps) => {
     </div>
   );
 
-  return <>{isModalOpen && createPortal(<ModalLayout />, document.body)}</>;
+  const DocumentsLayout = () => (
+    <div className={styles.documentsOverlay}>
+      <div className={styles.documentsContent}>
+        <div onClick={() => closeModal(modalKey)} className={styles.modalClose}>
+          <Image src="/svg/close.svg" alt="close" width={20} height={20} />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+
+  switch (modalKey) {
+    case 'error_message':
+    case 'application_feedback': {
+      return <>{isModalOpen && createPortal(<ModalLayout />, document.body)}</>;
+    }
+    default:
+      return (
+        <>{isModalOpen && createPortal(<DocumentsLayout />, document.body)}</>
+      );
+  }
 };
 
 export default Modal;
--- src/components/common/PDFViewer/PDFViewer.tsx
+++ src/components/common/PDFViewer/PDFViewer.tsx
@@ -38,12 +38,12 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ file }) => {
     setPages(numPages);
   }
 
-  const changePage = (newPageNumber: number) => {
-    setPageNumber(newPageNumber);
-    if (pageRef.current) {
-      pageRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+  // Const changePage = (newPageNumber: number) => {
+  //   SetPageNumber(newPageNumber);
+  //   If (pageRef.current) {
+  //     PageRef.current.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  // };
 
   return (
     <div className={styles.wrapper}>
@@ -52,17 +52,17 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ file }) => {
           <Page pageNumber={pageNumber} />
         </Document>
       </article>
-      <div className={styles.pageIndicator}>
+      {/* <div className={styles.pageIndicator}>
         {translations('page')} {pageNumber} {translations('from')} {pages}
-      </div>
-      <div className={styles.pagination}>
+      </div> */}
+      {/* <div className={styles.pagination}>
         {pageNumber > 1 && (
           <button onClick={() => changePage(pageNumber - 1)}>←</button>
         )}
         {pages && pageNumber + 1 <= pages && (
           <button onClick={() => changePage(pageNumber + 1)}>→</button>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
