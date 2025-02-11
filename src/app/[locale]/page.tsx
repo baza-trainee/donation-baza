1a21b6f804df4f62c6331ea8b17f301a04f55a48
O_Slavivna <133753511+oSlavivna@users.noreply.github.com>
2025-02-11 13:58:56 +0200

    feat: security (#84)
    * feat: security
    
    * refactor: add dynamic components
    
    * refactor: style cards grid
    
    * feat: add responsive without 420
    
    * feat: збереження змін перед pull --rebase
    
    * feat: add adaptives
    
    * fix: spacing
    
    * fix: mobile versions
    
    * fix: rollback dependencies
    
    * chore: add support test image and enable SecondViewSection in Home component
    
    ---------
    
    Co-authored-by: Maxym Biriukov <biryukov.mm@gmail.com>
    Co-authored-by: oSlavivna <hotyninfo@gmail.comm>

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,14 @@
 import BannerSection from '@/features/home/sections/BannerSection';
-import ButtonsExample from '@/components/examples/ButtonsExample';
+// Import ButtonsExample from '@/components/examples/ButtonsExample';
 import ContactUsFormSection from '@/features/home/sections/ContactUsFormSection';
 import ErrorMessage from '@/components/common/ErrorMessage/ErrorMessage';
 import EventsSection from '@/features/home/sections/EventsSection';
 import FirstViewSection from '@/features/home/sections/FirstViewSection';
-import HelpNowSection from '@/features/home/sections/HelpNowSection';
+// Import HelpNowSection from '@/features/home/sections/HelpNowSection';
 import Modal from '@/components/common/Modal/Modal';
 import QASection from '@/features/home/sections/QASection';
 import SecondViewSection from '@/features/home/sections/SecondViewSection';
-import TextExample from '@/components/examples/TextExample';
+// Import TextExample from '@/components/examples/TextExample';
 
 export default function Home() {
   return (
